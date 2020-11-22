a841562f34af12affd3a44b9e291565a3797914e
Vladimir Golovkin <vladimir.golovkin16@yandex.ru>
2020-11-22 17:13:02 +0500

    add: routing package

--- software-engineering/src/main.js
+++ software-engineering/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+import Auth from "../components/Auth"
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name:'auth',
+      component: Auth,
+    }
+  ]
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
